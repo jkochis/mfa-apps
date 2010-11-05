8a7712d8f8c6edff4d5513e88230890131f9567b
rbrecher <rbrecher@genuineinteractive.com>
2010-11-05 10:30:49 -0400

    Adding the ability to load tours via file sharing in iTunes, take two.

---
+++ Classes/FileSharingManager.h
@@ -0,0 +1,18 @@
+//
+//  FileSharingManager.h
+//  MFA Guide
+//
+//  Created by Robert Brecher on 11/1/10.
+//  Copyright 2010 Genuine Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FileSharingManager : NSObject {
+
+}
+
++ (void)checkBundles;
+
+@end
---
+++ Classes/FileSharingManager.m
@@ -0,0 +1,77 @@
+//
+//  FileSharingManager.m
+//  MFA Guide
+//
+//  Created by Robert Brecher on 11/1/10.
+//  Copyright 2010 Genuine Interactive. All rights reserved.
+//
+
+#import "FileSharingManager.h"
+
+#import "CoreDataManager.h"
+#import "Tour.h"
+#import "TourController.h"
+
+@implementation FileSharingManager
+
++ (void)checkBundles
+{
+	NSArray *documentsPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentsDirectory = [documentsPaths objectAtIndex:0];
+	NSFileManager *fileManager = [[NSFileManager alloc] init];
+	NSArray *contents = [fileManager contentsOfDirectoryAtPath:documentsDirectory error:nil];
+	if (contents) {
+		for (int i = 0; i < [contents count]; i++)
+		{
+			NSString *filename = [contents objectAtIndex:i];
+			if ([[filename pathExtension] isEqualToString:@"bundle"]) {
+				Tour *tour = [CoreDataManager getTourByBundleName:[filename stringByDeletingPathExtension]];
+				if (tour) {
+					NSDictionary *fileAttributes = [fileManager attributesOfItemAtPath:[documentsDirectory stringByAppendingPathComponent:filename] error:nil];
+					if ([[fileAttributes fileModificationDate] timeIntervalSinceDate:[tour updatedDate]] >= 0) {
+						[CoreDataManager updaterTourUpdatedDate:[fileAttributes fileModificationDate] byId:[tour id]];
+					}
+				}
+				else {
+					NSBundle *tourBundle = [NSBundle bundleWithPath:[documentsDirectory stringByAppendingPathComponent:filename]];
+					NSString *tourDataPath = [tourBundle pathForResource:TOUR_FILENAME ofType:@"xml"];
+					if (!tourDataPath) {
+						NSLog(@"%@: Could not find %@.xml", filename, TOUR_FILENAME);
+						continue;
+					}
+					NSNumber *tourId;
+					NSString *title;
+					NSString *language;
+					xmlDocPtr tourDoc = xmlParseFile([tourDataPath UTF8String]);
+					xmlNodePtr idNode = [TourMLUtils getIdInDocument:tourDoc];
+					if (idNode) {
+						char* idChars = (char*)xmlNodeGetContent(idNode);
+						tourId = [NSNumber numberWithInt:[[NSString stringWithUTF8String:idChars] intValue]];
+						free(idChars);
+					}
+					xmlNodePtr titleNode = [TourMLUtils getTitleInDocument:tourDoc];
+					if (titleNode) {
+						char* titleChars = (char*)xmlNodeGetContent(titleNode);
+						title = [NSString stringWithUTF8String:titleChars];
+						free(titleChars);
+					}
+					xmlNodePtr languageNode = [TourMLUtils getLanguageInDocument:tourDoc];
+					if (languageNode) {
+						char* languageChars = (char*)xmlNodeGetContent(languageNode);
+						language = [NSString stringWithUTF8String:languageChars];
+						free(languageChars);
+					}
+					xmlFreeDoc(tourDoc);
+					[CoreDataManager addOrUpdateTourWithId:tourId 
+													 title:title 
+												bundleName:[filename stringByDeletingPathExtension] 
+												  language:language 
+											   updatedDate:[NSDate date] 
+													errors:NO];
+				}
+			}
+		}
+	}
+}
+
+@end
